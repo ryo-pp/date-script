6baf9dfe98b43ee4177f54335bf74b6ad82d7c9a
KAZ <KAZ@KAZ-PC.local>
2013-05-02 02:13:49 +0900

    remove dump

--- DateModule.php
+++ DateModule.php
@@ -4,7 +4,6 @@ class DateModule
     public function valiableDate($date = "")
     {
         preg_match('/^([0-9]|[0-9]{2}):[0-9]{2}$/i', $date, $mathes);
-        var_dump($mathes);
         if (empty($date)) {
             return false;
         }
